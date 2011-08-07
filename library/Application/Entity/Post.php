c630c3c184aa418927bda4ef1040ddc37f274fda
Koji Ando <miau.jp@gmail.com>
2011-08-08 05:23:37 +0900

    generate accessor methods of the entity class
    php bin/doctrine.php orm:generate-entities library

--- library/Application/Entity/Post.php
+++ library/Application/Entity/Post.php
@@ -16,4 +16,94 @@ class Post
     protected $created;
     /** @Column(type="datetime", nullable=true) */
     protected $modified;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set text
+     *
+     * @param text $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * Get text
+     *
+     * @return text 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set created
+     *
+     * @param datetime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * Get created
+     *
+     * @return datetime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param datetime $modified
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return datetime 
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
 }
